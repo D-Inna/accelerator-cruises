722db3c2e025d9f706d26589417edc2fd0a34b47
Дементьева Инна <acorp.murka@gmail.com>
2023-03-29 19:24:14 +0300

    Добавляет бургер меню

---
+++ source/js/modules/burger-menu.js
@@ -0,0 +1,33 @@
+const burgerMenu = () => {
+  const iconHeader = document.querySelector('[data-burger]');
+  const navHeader = document.querySelector('[data-nav]');
+  const imageHeader = document.querySelector('[data-image]');
+
+  if (iconHeader) {
+    iconHeader.addEventListener('click', function () {
+      document.body.classList.toggle('[data-lock]');
+      iconHeader.classList.toggle('is-open');
+      navHeader.classList.toggle('is-open');
+      imageHeader.classList.toggle('is-open');
+    });
+  }
+
+  navHeader.addEventListener('click', function () {
+    if (iconHeader.classList.contains('is-open')) {
+      document.body.classList.remove('[data-lock]');
+      iconHeader.classList.remove('is-open');
+      navHeader.classList.remove('is-open');
+      imageHeader.classList.remove('is-open');
+    }
+  });
+
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      document.querySelector('[data-nav]').classList.remove(('is-open'));
+      document.querySelector('[data-burger]').classList.remove(('is-open'));
+      document.querySelector('[data-image]').classList.remove('is-open');
+    }
+  });
+};
+export {burgerMenu};
