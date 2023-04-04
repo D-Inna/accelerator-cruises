65a60834f44b46378e3b7a5c19ca4531967a2b49
Дементьева Инна <acorp.murka@gmail.com>
2023-04-04 15:25:47 +0300

    Меняет формат картинок

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+// import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {burgerMenu} from './modules/burger-menu';
 
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    // initModals();
     burgerMenu();
 
     const form = new Form();
